e943bda3775e2dcd21d0372d520cc0e4f87ee451
hh <thatbmr64@gmail.com>
2026-03-24 23:23:09 +0000

    Delete fsociety-main directory

--- fsociety-main/fsociety3/scripts/requirements.py
+++
@@ -1,6 +0,0 @@
-def install_requirements(distro):
-    if distro == "arch":
-        os.system('sudo pacman -S nmap yay')
-        os.system('yay -S android-sdk-platform-tools')
-    if distro == "ubuntu" or distro == "debian":
-        os.system('sudo apt-get install nmap android-platform-tools')
